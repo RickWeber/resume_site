ec2c5046e71728a61193745a888dec35be98ae6d
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-05-10 13:10:10 -0400

    setting up home app

--- resume_site/urls.py
+++ resume_site/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('linkedin/', RedirectView.as_view(url='https://linkedin.com/in/another-rick-weber')),
+    path('github/', RedirectView.as_view(url='https://github.com/RickWeber')),
 ]
