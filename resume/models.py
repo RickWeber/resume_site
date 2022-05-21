5d8db5f3bca87c0898f9159c50279aac9b4bcb92
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-05-21 17:14:26 -0400

    Allow duties to be reordered

--- resume/models.py
+++ resume/models.py
@@ -27,6 +27,7 @@ class Duties(models.Model):
     description = models.CharField(max_length=250)
     #job = models.ForeignKey(Experience, on_delete=models.CASCADE)
     job = models.ManyToManyField(Experience)
+    order = models.IntegerField()
     def __str__(self):
         return self.short_description
 
