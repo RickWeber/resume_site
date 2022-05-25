356b8091a12007cd3501822877bd26c18f976df4
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-05-25 10:18:24 -0400

    pass context data

--- wordle_search/urls.py
+++ wordle_search/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='wordle_search'
 urlpatterns = [
     path('', views.HomeView.as_view(), name="wordle_home"),
+    path('cheat', views.CheatView.as_view(), name="cheat"),
 #    path('word/<int:pk>', views.DayView.as_view(), name="wordle_day"),
 ]
--- wordle_search/views.py
+++ wordle_search/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from django.contrib import messages
 from .forms import GuessForm
 from .models import (
@@ -12,22 +12,41 @@ from .models import (
 # Create your views here.
 
 class HomeView(TemplateView):
-    template_name='wordle/home.html'
+    template_name='wordle_search/home.html'
     def get(self, request):
+        game = Game()
         form = GuessForm()
-        ctx = {'form': form}
+        ctx = {'form': form, 
+        'possible_words': game.show_possible_words(), 
+        'game': game}
         return render(request, self.template_name, ctx)
 
     def post(self, request):
+        game = request.game
         form = GuessForm(request.POST)
+        if form.is_valid():
+            game.make_guess(form.guess)
+            ctx = {'form': form,
+            'possible_words': game.show_possible_words(),
+            'game': game}
         if not form.is_valid():
-            ctx = {'form': form}
+            ctx = {'form': form,
+            'possible_words': game.show_possible_words(),
+            'game': game}
             return render(request, self.template_name, ctx)
 
         messages.add_message(request, messages.SUCCESS, 'Guess accepted.')
         return render(request, self.template_name, ctx)
         #return redirect(reverse('wordle_search:wordle_home'))
 
+
+class CheatView(DetailView):
+    model = TodaysAnswer
+    template_name = "wordle_search/cheat.html"
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["answer"] = TodaysAnswer().word
+        return context
 
 # TODO:
 # * Get the form template together
